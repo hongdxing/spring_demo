7f3571678e3d8cb52e36d2c5304164f9b2327f2b
hongxing <lockeduan@outlook.com>
2022-05-29 16:31:18 +0800

    update

--- configserver/config-client/src/main/java/com/hongxing/configclient/ConfigClientApplication.java
+++ configserver/config-client/src/main/java/com/hongxing/configclient/ConfigClientApplication.java
@@ -1,13 +1,19 @@
 package com.hongxing.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ConfigClientApplication {
 
-	public static void main(String[] args) {
+
+
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ConfigClientApplication.class, args);
+
 	}
 
 }
--- configserver/config-client/src/main/java/com/hongxing/configclient/TestController.java
+++ configserver/config-client/src/main/java/com/hongxing/configclient/TestController.java
@@ -1,2 +1,20 @@
-package com.hongxing.configclient;public class TestController {
+package com.hongxing.configclient;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/env")
+public class TestController {
+
+    @Value("${server.env}")
+    private String env;
+
+    @GetMapping
+    public String getEnv(){
+        return env;
+    }
 }
