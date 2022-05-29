f4bab1a9a3631b6388d67ddf076e9cf121c02119
hongxing <lockeduan@outlook.com>
2022-05-29 12:56:24 +0800

    first commit

---
+++ configserver/config-client/src/test/java/com/hongxing/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongxing.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
